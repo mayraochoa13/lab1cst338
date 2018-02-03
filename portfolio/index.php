2f4e9a1e70798dd82cc4aabd98626422d3bbfa35
Mayra Ochoa <mayochoa@csumb.edu>
2018-02-03 03:55:17 +0000

    HW CST-336

---
+++ portfolio/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8"/>
+        <title>Mayra Ochoa: Personal Website</title>
+        <!-- comment>
+        <style>
+            body{
+                background-color: black;
+                color: white;
+            }
+            </style>
+        <!-->
+        <link href = "css/style.css" rel ="stylesheet" type ="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+    </head>
+    
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <strong>
+                <h1>Mayra Ochoa</h1>
+            </strong>
+        </header>
+        <nav>
+            <hr width = "100%"/>
+            <a href = "index.php">Home</a>
+            <a href = "about.html">About</a>
+            <a href = "contact.html">Contact</a>
+        </nav>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+     
+        
+      
+        <main>
+            <figure id="me">
+                <img width = "400px" height = "500px" src = "img/mee.jpg"/>
+            </figure>
+            <div id="WelcomeText">
+                <br /><br /><br />
+                Hello!<br/>
+                <p>
+                    Thank you for visiting my professional portfolio website.
+                </p>
+                <p>
+                    Currently, I am a student majoring in Software Engineering at California State University Monterey Bay.
+                </p>
+                <p>
+                    Feel free of contact me.
+                </p>
+                <br /><br />
+                <em>"Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin" </em><br />
+                  - Mother Teresa
+            </div>
+        </main>
+        <footer>
+            <hr>
+                <figure id="csumb">
+                <img width = "200 px" height = "150 px" src = "https://experiencecle.com/wp-content/uploads/2016/03/csumb_logo-1.png"/><br/>
+                 CST 336 Internet Programming. 2017© Ochoa
+            </figure>
+        </footer>
+    </body>
+    <!-- closing body -->
+
+</html>
